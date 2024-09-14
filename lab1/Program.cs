623958fad736c0049a58f42a5c484e0c74086bba
dvulist <dvulistnik@gmail.com>
2024-09-14 11:26:24 +0300

    Add project files.

---
+++ lab1/Program.cs
@@ -0,0 +1,15 @@
+﻿using lab1;
+
+Student[] students = { new Student("Костя"), new Student("Дима", 19) };
+
+for (int i = 1; i < 11; i++)
+{
+    Console.WriteLine(i + " год");
+    foreach (Student student in students)
+    {
+        student.WriteInfo();
+        student.BecomeOlder();
+    }
+    Console.WriteLine();
+}
+
---
+++ lab1/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    public class Student
+    {
+        private string _name;
+        public int age { get; set; }
+        public Student(string name)
+        {
+            this._name = name;
+        }
+        public Student(string name, int age)
+        {
+            this._name = name;
+            this.age = age;
+        }
+
+        public void WriteInfo()
+        {
+            Console.WriteLine($"Имя: {_name}, Возраст: {age}");
+        }
+        public void BecomeOlder()
+        {
+            age--;
+        }
+    }
+}
