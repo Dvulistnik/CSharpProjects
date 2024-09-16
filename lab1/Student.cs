cbf01e4f24c77ca0a6814da007dc625f6e969d4a
dvulist <dvulistnik@gmail.com>
2024-09-16 22:03:47 +0300

    lab2. The ref modifier has not been implemented, since so far I have not found a use for it in the current version, but I understand how it works

--- lab1/Student.cs
+++ lab1/Student.cs
@@ -9,17 +9,23 @@ namespace lab1
     public class Student
     {
         private string _name;
-        public int age { get; set; }
+        public int age;
+
+        public Subject? favoriteSubject { get; set; }
+        public Game? favoriteGame { get; set; }
+
         public Student(string name)
         {
             this._name = name;
         }
-        public Student(string name, int age)
+        public Student(string name, int age, Subject? subject=null, Game? game=null)
         {
             this._name = name;
             this.age = age;
-        }
 
+            this.favoriteSubject = subject!;
+            this.favoriteGame = game!;
+        }
         public void WriteInfo()
         {
             Console.WriteLine($"Имя: {_name}, Возраст: {age}");
@@ -27,6 +33,12 @@ namespace lab1
         public void BecomeOlder()
         {
             age++;
+        }
+        
+        public void getFavoriteSubjectAndGame(out Subject s, out Game g)
+        {
+            s = favoriteSubject!;
+            g = favoriteGame!;
         }
     }
 }
