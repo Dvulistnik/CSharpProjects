1275cc131b68acb36508497b7dae835fd549df5c
dvulist <dvulistnik@gmail.com>
2024-09-20 17:49:53 +0300

    Lab3

---
+++ lab3/Program.cs
@@ -0,0 +1,18 @@
+﻿using lab3;
+
+Student.University = "СГУ";
+
+StudentManagement.Students.Add(new("Kola"));
+StudentManagement.Students.Add(new("Ira", 17));
+StudentManagement.Students.Add(new()
+{
+    Name = "Vika",
+    Age = 30,
+});
+
+foreach (Student s in StudentManagement.Students)
+{
+    s.WriteInfo();
+    Console.WriteLine();
+}
+
---
+++ lab3/StudentManagement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    public static class StudentManagement
+    {
+        public static List<Student> Students = new List<Student>();
+
+    }
+}
