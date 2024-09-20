9a87666a280dfe11bd6035ac9a8ad96269077c2f
dvulist <dvulistnik@gmail.com>
2024-09-20 18:05:35 +0300

    Lab4

---
+++ lab4/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    public abstract class Person
+    {
+        public abstract string Name { get; set; }
+        public virtual void WriteInfo()
+        {
+            Console.WriteLine("Пусто");
+        }
+    }
+}
