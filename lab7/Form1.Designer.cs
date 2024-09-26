d9eaeaa03f11d351cd40befc800d38460e2e57cc
dvulist <dvulistnik@gmail.com>
2024-09-26 12:54:21 +0300

    lab7 Attempts to make a customTextBox to be beautiful...

---
+++ lab7/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace lab7
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            customTextbox1 = new customTextbox();
+            dataGridView1 = new DataGridView();
+            customTextbox2 = new customTextbox();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(customTextbox2);
+            groupBox1.Controls.Add(customTextbox1);
+            groupBox1.Dock = DockStyle.Bottom;
+            groupBox1.Location = new Point(0, 291);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(800, 159);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Info";
+            // 
+            // customTextbox1
+            // 
+            customTextbox1.Location = new Point(107, 26);
+            customTextbox1.Name = "customTextbox1";
+            customTextbox1.Size = new Size(272, 54);
+            customTextbox1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.Location = new Point(0, 0);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(800, 291);
+            dataGridView1.TabIndex = 1;
+            // 
+            // customTextbox2
+            // 
+            customTextbox2.Location = new Point(107, 85);
+            customTextbox2.Name = "customTextbox2";
+            customTextbox2.Size = new Size(326, 68);
+            customTextbox2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dataGridView1);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private DataGridView dataGridView1;
+        private customTextbox customTextbox1;
+        private customTextbox customTextbox2;
+    }
+}
---
+++ lab7/customTextbox.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab7
+{
+    public partial class customTextbox : UserControl
+    {
+        public customTextbox()
+        {
+            InitializeComponent();
+        }
+
+        private void labelTimer_Tick(object sender, EventArgs e)
+        {
+            int y = label1.Location.Y;
+
+            if (textBox1.Focused)
+            {
+                y -= 2;
+                label1.Location = new Point(label1.Location.X, y);
+
+                if (y <= 0)
+                {
+                    labelTimer.Stop();
+                    label1.Font = new Font("Segoe UI", 8);
+                    y = 0;
+                    label1.ForeColor = Color.Silver;
+                }
+            }
+            else
+            {
+                y += 2;
+                label1.Location = new Point(label1.Location.X, y);
+                if (y >= 22)
+                {
+                    labelTimer.Stop();
+                    label1.Font = new Font("Segoe UI", 10);
+                    y = 22;
+                    label1.ForeColor = Color.Black;
+                }
+            }
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                labelTimer.Start();
+            }
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                labelTimer.Start();
+            }
+        }
+    }
+}
